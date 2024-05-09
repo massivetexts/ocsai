6b6f7e345e9012014f4876067b893cc2c9ad7e30
Peter Organisciak <organisciak@gmail.com>
2024-05-08 20:35:09 -0600

    Add multi-test variant of classic prompter

--- ocsai/train/__init__.py
+++ ocsai/train/__init__.py
@@ -3,3 +3,4 @@ from .llm_base_prompter import LLM_Base_Prompter
 from .gpt_ocsai2_prompter import GPT_Ocsai2_Prompter
 from .gpt_classic_prompter import GPT_Classic_Prompter
 from .gpt_classic_chat_prompter import GPT_Classic_Chat_Prompter
+from .gpt_classic2_prompter import GPT_Classic2_Prompter
---
+++ ocsai/train/gpt_classic2_prompter.py
@@ -0,0 +1,28 @@
+from .gpt_classic_prompter import GPT_Classic_Prompter
+
+
+class GPT_Classic2_Prompter(GPT_Classic_Prompter):
+    ''' A slightly modified training format from the 'classic' format in
+    Organisciak et al. 2023, used in Acar et al. 2023, adjusted to support
+    multiple task types.
+    '''
+    max_tokens: int = 2
+
+    def craft_prompt(self, item: str, response: str,
+                     task_type: str | None = 'uses', question=None,
+                     language='eng'):
+        # prompt templates should take 2 args - item and response
+        known_task_type_codes = ['Uses', 'Instances', 'Completion']
+        if task_type is None:
+            raise ValueError("Task type for classic prompter is necessary.")
+        task_type = task_type.capitalize()
+        if task_type not in known_task_type_codes:
+            self.logger.debug(f"Task type {task_type} not a known type.")
+        prompt_template = f'DT {task_type} Prompt:{item}\nResponse:{response}\nScore:\n'
+
+        if question:
+            self.logger.warning("Question is not supported with Classic Prompter")
+        if language is not None and language != 'eng':
+            self.logger.warning("Only 'eng' language is supported with Classic Prompter")
+
+        return prompt_template.format(item, response)
