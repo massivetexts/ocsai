5d1490039b7cd85a6319b5b686d4fbdb9fcb4ff3
Peter Organisciak <organisciak@gmail.com>
2024-07-11 16:21:47 -0600

    Update prompter init

--- ocsai/prompter/__init__.py
+++ ocsai/prompter/__init__.py
@@ -3,3 +3,4 @@ from .ocsai2_prompter import Ocsai2_Prompter  # noqa: F401
 from .ocsai1_prompter import Ocsai1_Prompter  # noqa: F401
 from .ocsai1_multi_prompter import Ocsai1_Multi_Prompter  # noqa: F401
 from .ocsai1_chat_prompter import Ocsai1_Chat_Prompter  # noqa: F401
+from .ocsai1_multi_chat_prompter import Ocsai1_Multi_Chat_Prompter  # noqa: F401
